76c70fd37fe81586c52aa5bca9ab113cb6196796
Carl <z.he17@newcastle.ac.uk>
2021-01-22 20:11:44 +0800

    Array 2

---
+++ src/Array2.java
@@ -0,0 +1,21 @@
+/******************
+ 5. 命令行参数
+ *
+ *
+ ******************/
+
+
+
+
+public class Array2 {
+    public static void main(String[] args) {
+        if (args.length == 0 || args[0].equals("-h"))
+            System.out.println("Hello");
+        else if (args[0].equals("-g"))
+            System.out.println("Goodbye,");
+
+        for (int i = 1; i < args.length; i++)
+            System.out.println(" " +  args[i]);
+        System.out.println("!");
+    }
+}
