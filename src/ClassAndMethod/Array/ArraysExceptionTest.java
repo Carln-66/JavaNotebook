f7206313c936ea82353595eefaac720a7c7b2baa
Carl <carln@foxmail.com>
2021-01-27 20:02:50 +0800

    Arrays Exception Test

---
+++ src/ClassAndMethod/Array/ArraysExceptionTest.java
@@ -0,0 +1,34 @@
+package ClassAndMethod.Array;
+
+/***
+ *  数组中的常见异常
+ *  1. 数组角标越界的异常：ArrayIndexOutOfBoundsException
+ *
+ *  2. 空指针异常：NullPointerException
+ *
+ */
+
+public class ArraysExceptionTest {
+    public static void main(String[] args) {
+
+        //数组角标越界的异常：ArrayIndexOutOfBoundsException
+//        int[] arr = new int[]{1, 2, 3, 4, 5};
+//        for (int i = 0; i <= arr.length; i++)       //角标越界
+//        {
+//            System.out.println(arr[i]);
+//        }
+
+        //空指针异常：NullPointerException
+//        int[] arr1 = new int[]{1, 2, 3};
+//        arr1 = null;
+//        System.out.println(arr1[0]);
+
+//        int[][] arr2 = new int[4][];
+//        System.out.println(arr2[0][0]);
+
+//        String[] arr3 = new String[]{"AA","BB","CC"};
+//        arr3[0] = null;
+//        System.out.println(arr3[0].toString());
+
+    }
+}
