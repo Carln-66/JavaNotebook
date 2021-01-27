7daed750d0d5338dd53909e9fa89a49c15908cab
Carl <carln@foxmail.com>
2021-01-27 13:01:07 +0800

    Control Flow

--- src/ClassAndMethod/ControlFlow/ControlFlow.java
+++ src/ClassAndMethod/ControlFlow/ControlFlow.java
@@ -3,36 +3,36 @@ package ClassAndMethod.ControlFlow;
 import java.util.Scanner;
 
 /******************************************
- 控制流程
+    控制流程
 
- 1. 条件语句
+    1. 条件语句
  Java中，条件语句的形式位
-    if (condition) statement
+        if (condition) statement
  Java中常常希望在某个条件位真的时候执行多条语句。在这种情况下，就可以使用块语句(block statement)，形式为
- {
- statement1
- statement2
- . . .
- }
+        {
+        statement1
+        statement2
+        . . .
+        }
  例如
- if (yourSales >= target)
- {
-    performance = "satisfactory";       //当yourSale大于或等于target时，执行大括号中的所有语句
-    bonus = 200;
- }
+        if (yourSales >= target)
+        {
+            performance = "satisfactory";       //当yourSale大于或等于target时，执行大括号中的所有语句
+            bonus = 200;
+        }
 
  Java中，更一般的条件语句如下所示
-    if (condition) statement1
-    else statement2
+        if (condition) statement1
+        else statement2
  else部分总是可选的。else子句与最邻近的if构成一组
 
- 2. 循环语句
+    2. 循环语句
  当条件为true时，while循环执行一条语句（也可以是一个块语句）。一般形式如下
-    while (condition) statement
- 若开始时循环条件的值为false，那么while循环一次也不执行
- * 如果希望循环体至少执行一次需要使用do/while循环将检测放在最后。语法如下：
-    do statement while (condition);
- 这种循环语句先执行语句（通常是一个语句块），然后再检测循环条件。如果为true则重复执行语句，然后再次检测循环条件，以此类推。
+        while (condition) statement
+    若开始时循环条件的值为false，那么while循环一次也不执行
+ *  如果希望循环体至少执行一次需要使用do/while循环将检测放在最后。语法如下：
+        do statement while (condition);
+    这种循环语句先执行语句（通常是一个语句块），然后再检测循环条件。如果为true则重复执行语句，然后再次检测循环条件，以此类推。
  *****************************************/
 
 //程序计算需要多长时间才能够储存一定数量的退休金，假定每年存入数量相同的金额，而且利率是固定的。
