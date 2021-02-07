227c11b1957708dcc8860d715086207cefd583f2
Carl <carln@foxmail.com>
2021-02-07 10:30:43 +0800

    Thread Pool

--- JavaSenior/src/Thread/ThreadDoc/ThreadPool.java
+++ JavaSenior/src/Thread/ThreadDoc/ThreadPool.java
@@ -45,7 +45,6 @@ public class ThreadPool {
         //2. 执行指定的线程的操作。需要提供实现Runnable接口或Callable接口的实现类的对象
         service.execute(new NumberThread());    //适用于Runnable
 //        service.submit();       //适用于Callable
-
         service.shutdown();     //3. 关闭连接池
     }
 }
