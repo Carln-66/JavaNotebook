60baae64f29f89d99e7df6efca48b76e20217e2a
Carl <carln@foxmail.com>
2021-02-07 21:31:15 +0800

    获取两字符串中最大相同字串

---
+++ JavaSenior/src/string_class/practice/getMaxSubString.java
@@ -0,0 +1,24 @@
+package string_class.practice;
+
+/**
+ * @Auther: Carl
+ * @Date: 2021/02/07/21:22
+ * @Description: 获取两个字符串中最大相同子串。比如：
+ *    str1 = "abcwerthelloyuiodef“;str2 = "cvhellobnm"//10
+ *    提示：将短的那个串进行长度依次递减的子串与较长的串比较。
+ */
+public class getMaxSubString {
+
+    //未完成
+    public String test(String str1, String str2){
+        if (str2.length() < str1.length()){
+            int index = 0;
+
+
+        }
+        String s = str1;
+        str1 = str2;
+        str1 = s;
+        return null;
+    }
+}
