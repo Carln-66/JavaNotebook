1e40cbb17006a5f087d9e0c4cb360d94beba5823
Carl <carln@foxmail.com>
2021-02-09 20:41:05 +0800

    Annotation 注解

--- JavaSenior/src/AnnotationTest/AnnotationTest.java
+++ JavaSenior/src/AnnotationTest/AnnotationTest.java
@@ -25,7 +25,22 @@ package AnnotationTest;
  *  ④ 如果自定义注解没有成员，表明是一个标识作用
  *
  *  如果注解有成员，在使用注解时，需要指明成员的值(参照MyAnnotation构造)
+ *  自定义注解必须配上注解的信息处理流程(使用反射)才有意义
+ *  自定义注解通常都会指明两个元注解：Retention、Target
  *
+ *  4. JDK提供的四种元注解：对现有的注解进行解释说明的注解
+ *      Retention: 指定的所修饰的Annotation的生命周期：SOURCE、CLASS(默认行为)、RUNTIME(只有声明为RUNTIME生命周期的注解才能通过反射获取)
+ *      Target: 用于指定被修饰的Annotation能用于修饰哪些程序元素
+ *      //以下两种元注解出现频率较低
+ *      Documented: 表示所修饰的注解被javadoc解析时，保留下来
+ *      Inherited: 被它修饰的Annotation将具有继承性
+ *
+ *  5. 通过反射获取注解信息
+ *  6. JDK8中注解的新特性：可重复注解、类型注解
+ *      可重复注解：在MyAnnotation声明@Repetable，成员值为MyAnnotations.class
+ *                MyAnnotation的Target和Retention等元注解和MyAnnotations相同
+ *      类型注解：ElementType.TYPE_PARAMETER表示该注解能写在类型变量的声明语句中(如泛型声明)
+ *              ElementType.TYPE_USE表示该注解能写在适用类型的任何语句中。
  *
  *
  */
