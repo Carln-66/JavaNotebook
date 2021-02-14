a1cf6d021b8581c389f6da37086bf677edbacca2
Carl <carln@foxmail.com>
2021-02-14 19:06:40 +0800

    泛型 Generic

---
+++ JavaSenior/src/GenericTest/genric_class_test1/Student.java
@@ -0,0 +1,12 @@
+package GenericTest.genric_class_test1;
+
+import CollectionTest.questiontest.Person;
+
+/**
+ * @Auther: Carl
+ * @Date: 2021/02/14/14:53
+ * @Description:
+ */
+public class Student extends Person {
+
+}
