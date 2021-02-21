29f5aacc285be8ef5942d925292330cef288559d
Carl <carln@foxmail.com>
2021-02-21 19:59:36 +0800

    reflection 反射

---
+++ JavaSenior/Reflection/src/ReflectionTest.java
@@ -0,0 +1,75 @@
+import exer.Person;
+import org.junit.Test;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+/**
+ * @Auther: Carl
+ * @Date: 2021/02/21/18:01
+ * @Description:
+ *  调用运行时类的指定结构：属性、方法、构造器
+ */
+public class ReflectionTest {
+
+    //对属性要求过于严格，故而不需要掌握
+    @Test
+    public void testField() throws NoSuchFieldException, IllegalAccessException, InstantiationException {
+        Class aClass = Person.class;
+
+        //创建运行时类的对象
+        Person p = (Person) aClass.newInstance();
+
+        //获取指定的属性：要求运行时类的属性声明为public
+        //由于属性值很少设置为public，因而如下方法通常不使用
+        Field id = aClass.getField("id");
+
+        //设置当前属性的值
+        //set(): 参数1：指明设置哪个对象的属性    参数2：将此属性值设置为多少
+        id.set(p, 1234);
+
+        //获取当前属性的值
+        int pId = (int) id.get(p);
+        System.out.println(pId);
+    }
+
+    //如何获得运行时类中的指定的属性 -- 需要掌握
+    @Test
+    public void testField1() throws IllegalAccessException, InstantiationException, NoSuchFieldException {
+        Class aClass = Person.class;
+
+        //创建运行时类的对象
+        Person p = (Person) aClass.newInstance();
+
+        //1. getDeclaredField(String fieldName): 获取运行时类中指定变量名的属性
+        Field name = aClass.getDeclaredField("name");
+
+        //2. 保证当前属性是可访问的
+        name.setAccessible(true);
+
+        //3. 获取、设置指定对象的此属性值
+        name.set(p, "李四");
+        System.out.println(name.get(p));
+    }
+
+    //如何操作运行时类中的指定方法 -- 需要掌握
+    @Test
+    public void testMethod() throws Exception {
+        Class aClass = Person.class;
+
+        //创建运行时类的对象
+        Person p = (Person) aClass.newInstance();
+
+        //1. 获取指定的某个方法
+        //getDeclaredMethod(): 参数1：指明获取的方法的名称   参数2：指明获取的方法的形参列表
+        Method declaredMethod = aClass.getDeclaredMethod("show", String.class);
+
+        //2. 保证当前方法是可访问的
+        declaredMethod.setAccessible(true);
+
+        //3. 调用invoke(): 参数1：方法的调用者  参数2：给方法形参赋值的实参
+        //invoke()的返回值与对应类中调用的方法的返回值相同
+        Object invoke = declaredMethod.invoke(p, "CHINA");
+
+    }
+}
--- JavaSenior/Reflection/src/exer1/OtherTest.java
+++ JavaSenior/Reflection/src/exer1/OtherTest.java
@@ -3,7 +3,10 @@ package exer1;
 import exer.Person;
 import org.junit.Test;
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Constructor;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 
 /**
  * @Auther: Carl
@@ -28,6 +31,85 @@ public class OtherTest {
             for (Constructor c1 : declaredConstructors) {
                 System.out.println(c1);
             }
+        }
+    }
+
+    /*
+        获取运行时类的父类
+     */
+    @Test
+    public void test2(){
+        Class aClass = Person.class;
+        Class superclass = aClass.getSuperclass();
+        System.out.println(superclass);
+    }
+
+    /*
+        获取运行时类的带泛型的父类
+     */
+    @Test
+    public void test3(){
+        Class aClass = Person.class;
+        Type genericSuperclass = aClass.getGenericSuperclass();
+        System.out.println(genericSuperclass);
+    }
+
+    /*
+        获取运行时类的带泛型的泛型
+
+        代码：
+        逻辑性代码
+        功能性代码
+     */
+    @Test
+    public void test4(){
+        Class aClass = Person.class;
+
+        Type genericSuperclass = aClass.getGenericSuperclass();
+        ParameterizedType parameterizedType = (ParameterizedType) genericSuperclass;
+        //获取泛型类型
+        Type[] actualTypeArguments = parameterizedType.getActualTypeArguments();
+//        System.out.println(actualTypeArguments[0].getTypeName());
+        System.out.println(((Class) actualTypeArguments[0]).getName());
+    }
+
+    /*
+        获取运行时类实现的接口
+     */
+    @Test
+    public void test5(){
+        Class aClass = Person.class;
+        Class[] interfaces = aClass.getInterfaces();
+        for (Class c : interfaces){
+            System.out.println(c);
+        }
+
+        //获取运行时类的父类实现的接口
+        Class[] interfaces1 = aClass.getSuperclass().getInterfaces();
+        for (Class c : interfaces1){
+            System.out.println(c);
+        }
+    }
+
+    /*
+        获取运行时类所在的包
+     */
+    @Test
+    public void test6(){
+        Class aClass = Person.class;
+        Package aPackage = aClass.getPackage();
+        System.out.println(aPackage);
+    }
+
+    /*
+        获取运行时类声明的注解
+    */
+    @Test
+    public void test7(){
+        Class aClass = Person.class;
+        Annotation[] annotations = aClass.getAnnotations();
+        for (Annotation annos : annotations) {
+            System.out.println(annos);
         }
     }
 }
