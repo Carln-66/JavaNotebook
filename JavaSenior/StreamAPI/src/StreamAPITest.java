17f28b12aa8403bc98fdf463e655202137dbf4e5
Carl <carln@foxmail.com>
2021-02-24 21:50:27 +0800

    update

--- JavaSenior/StreamAPI/src/StreamAPITest.java
+++ JavaSenior/StreamAPI/src/StreamAPITest.java
@@ -1,3 +1,12 @@
+import MethodReferences.Employee;
+import MethodReferences.EmployeeData;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 /**
  * @Auther: Carl
  * @Date: 2021/02/24/16:28
@@ -14,7 +23,54 @@
  *      4.1 一个操作数据链，对数据源的数据进行处理
  *      4.2 一旦执行终止操作，就执行中间操作链，并产生结果。之后不会再被使用
  *
+ *      测试Stream的实例化
+ *
  *
  */
 public class StreamAPITest {
+
+    //创建Stream方式一：通过集合
+    @Test
+    public void test1(){
+        List<Employee> employees = EmployeeData.getEmployees();
+
+        //default Stream<E> stream(): 返回一个顺序流
+        Stream<Employee> stream = employees.stream();
+
+        //default Stream<E> parallelStream: 返回一个并行流
+        Stream<Employee> parallelStream = employees.parallelStream();
+    }
+
+    //创建Stream方式二：通过数组
+    @Test
+    public void test2(){
+        int[] arr = new int[]{1, 2, 3, 4, 5, 6};
+        //调用Arrays类的static<T> Stream<T> stream(T[] array): 返回一个流
+        IntStream stream = Arrays.stream(arr);
+        Employee employee1 = new Employee(100, "张三");
+        Employee employee2 = new Employee(101, "李四");
+        Employee employee3 = new Employee(102, "王五");
+        Employee[] arr1 = new Employee[]{employee1, employee2, employee3};
+
+        Stream<Employee> stream1 = Arrays.stream(arr1);
+    }
+
+    //创建Stream方式三：通过Stream的of()
+    @Test
+    public void test3(){
+        Stream<Integer> integerStream = Stream.of(1, 2, 3, 4, 5, 6);
+    }
+
+    //创建Stream方式四：创建无限流
+    @Test
+    public void test4(){
+        //迭代
+        //public static<T> Stream<T> iterate(final T seed, final UnaryOperator<T> f)
+        //遍历前10个偶数
+        Stream.iterate(0, t -> t + 2).limit(10).forEach(System.out :: println);
+        //生成
+        //public static<T> Stream<T> generate(Supplier<T> s)
+        Stream.generate(Math :: random).limit(10).forEach(System.out :: println);
+    }
+
 }
---
+++ JavaSenior/StreamAPI/src/StreamAPITest1.java
@@ -0,0 +1,55 @@
+import MethodReferences.Employee;
+import MethodReferences.EmployeeData;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+/**
+ * @Auther: Carl
+ * @Date: 2021/02/24/19:31
+ * @Description:
+ *  测试Stream的中间操作
+ */
+public class StreamAPITest1 {
+
+    //1. 筛选与切片
+    @Test
+    public void test1(){
+        List<Employee> list = EmployeeData.getEmployees();
+        //filter(Predicate p) -- 接收Lambda，从中排除某些元素
+        Stream<Employee> stream = list.stream();
+        //查询员工表中薪资大于7000的员工信息
+        stream.filter(e -> e.getSalary() > 7000).forEach(System.out::println);
+
+        System.out.println();
+        //limit(n) -- 截断流，使其元素不超过给定数量
+        list.stream().limit(3).forEach(System.out::println);
+
+        System.out.println();
+        //skip(n) -- 跳过元素，返回一个扔掉了前n个元素的流，若流中元素不足n个，则返回一个空流。与limit(n)互补
+        list.stream().skip(100).forEach(System.out::println);
+
+        System.out.println();
+        //distinct() -- 筛选，通过流所声称元素的hashCode()和equals()去除重复元素
+        list.add(new Employee(1010, "马化腾", 22, 6520.38));
+        list.add(new Employee(1010, "马化腾", 22, 6520.38));
+        list.add(new Employee(1010, "马化腾", 22, 6520.38));
+        list.add(new Employee(1010, "马化腾", 22, 6520.38));
+        list.add(new Employee(1010, "马化腾", 22, 6520.38));
+
+        list.stream().distinct().forEach(System.out::println);
+    }
+
+    //映射
+    @Test
+    public void test2(){
+        //map(Function f) -- 接收一个函数作为参数，将元素转换成其他形式或提取信息，该函数会被应用到每一个新的元素
+        List<String> list = Arrays.asList("aa", "bb", "cc", "dd");
+        list.stream().map(str -> str.toUpperCase()).forEach(System.out::println);
+        //练习
+
+        //flatMap(Function f) -- 接收一个函数作为参数，将流中每个值都换成另一个流
+    }
+}
